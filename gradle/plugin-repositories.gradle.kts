8f033967ed2fc76a04fd21e4823c4060566f1925
Kirill Boyarshinov <git@kboyarshinov.dev>
2024-03-10 13:18:12 +0100

    cleanup android plugin dependencies

--- gradle/plugin-repositories.gradle.kts
+++ gradle/plugin-repositories.gradle.kts
@@ -12,19 +12,6 @@ pluginManagement {
             }
         }
 
-        // Allow Google Android plugins from google repository
-        exclusiveContent {
-            forRepository { google() }
-
-            filter {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("androidx.*")
-
-                // needed for Android Gradle Plugin
-                includeGroup("com.google.testing.platform")
-            }
-        }
-
         // Maven central for the rest of plugins
         mavenCentral()
     }
--- gradle/repositories.gradle.kts
+++ gradle/repositories.gradle.kts
@@ -1,15 +1,6 @@
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
-        exclusiveContent {
-            forRepository { google() }
-
-            filter {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("androidx\\..*")
-                includeGroupByRegex("android\\..*")
-            }
-        }
         mavenCentral()
     }
 }
