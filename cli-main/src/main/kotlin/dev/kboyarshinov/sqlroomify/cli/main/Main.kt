d486e1be34f00f84b674b13be1d2933cd39e04f0
Kirill Boyarshinov <git@kboyarshinov.dev>
2024-03-10 16:36:53 +0100

    Implement Run command

--- cli-main/src/main/kotlin/dev/kboyarshinov/sqlroomify/cli/main/Main.kt
+++ cli-main/src/main/kotlin/dev/kboyarshinov/sqlroomify/cli/main/Main.kt
@@ -1,11 +1,41 @@
 package dev.kboyarshinov.sqlroomify.cli.main
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.types.file
+import com.github.ajalt.clikt.parameters.types.path
+import okio.Path.Companion.toOkioPath
+import java.io.File
+import java.nio.file.Path
 
-class Hello : CliktCommand() {
+class Run : CliktCommand() {
+    val input: File by argument(help = "Input SQL Schema file").file(
+        mustExist = true, mustBeReadable = true
+    )
+    val output: Path by argument(help = "Output folder for generated files").path(
+        mustExist = false,
+        canBeFile = false,
+    )
+    val packageName: String by argument(name = "package-name", help = "Package name to use for generated files")
+
     override fun run() {
-        echo("Hello sqlroomify!")
+        echo("Processing input: ${input.path}...")
+
+        val result = SqlRoomify.sqlToRoom(
+            input = input.toOkioPath(),
+            outputDir = output.toOkioPath(),
+            outputPackage = packageName
+        )
+        when (result) {
+            is SqlRoomify.Success -> {
+                echo("Success! Generated files to ${output.toFile().path}")
+            }
+
+            is SqlRoomify.Error -> {
+                echo(result.message, err = true)
+            }
+        }
     }
 }
 
-fun main(args: Array<String>) = Hello().main(args)
+fun main(args: Array<String>) = Run().main(args)
